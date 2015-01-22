8fb5e3821ad317d7531adcf7fe535e99a5bddf31
Leonid Rozenberg <leonidr@gmail.com>
2015-01-22 16:46:57 -0500

    Simple now takes a step

--- simple.ml
+++ simple.ml
@@ -2,6 +2,9 @@
 #thread
 #require "ketrew"
 
+open Ketrew_pervasives ;;
+module Configuration = Ketrew_configuration ;;
+
 let run_local_shell cmd =
   let module KEDSL = Ketrew.EDSL in
   let program = KEDSL.Program.sh cmd in
@@ -9,18 +12,47 @@ let run_local_shell cmd =
   KEDSL.target "run_command_with_lsf" ~make:process
 
 
-let ()  =
+let unwrap = function
+  | `Ok c    -> c
+  | `Error _ -> failwith "unwrap"
+
+let run_first_arg () =
   (* Create the  workflow with the first argument of the command line: *)
   let workflow = run_local_shell Sys.argv.(1) in
   (* Then, `run` is the only function that “does” something, it submits the
      workflow to the engine: *)
-  let () = Ketrew.EDSL.run workflow in
+  Ketrew.EDSL.run workflow
   (* If Ketrew is in Standalone mode, this means writing the workflow in the
      database (nothing runs yet, you need to run Ketrew's engine yourself).
      If Ketrew is in Client-Server mode, this means sending the workflow to the
      server over HTTPS. The server will start running the workflow right away.  *)
-  let open Ketrew_pervasives in
-  let module Configuration = Ketrew_configuration in 
-  let cdef = Configuration.get_configuration Configuration.default_configuration_path in
-  let c = Lwt_main.run cdef in
-  ()
+
+let configuration () =
+    Configuration.get_configuration Configuration.default_configuration_path
+    |> Lwt_main.run
+    |> unwrap
+
+let standalone () =
+  let cdef = configuration () in
+  match Configuration.mode cdef with
+  | `Client _ -> failwith "Why Client ?"
+  | `Server _ -> failwith "Why Server ?"
+  | `Standalone s -> s
+
+let step ~client =
+  match Ketrew_client.get_local_engine client with
+  | None -> fail (`Failure "no client")
+  | Some state ->
+      begin
+        Ketrew_engine.step state >>=
+          fun foo ->
+            Printf.printf "steps: %d" (List.length foo);
+            return ()
+      end
+
+let ()  =
+  let () = if (Array.length Sys.argv) >= 2 then run_first_arg () in
+  let c = configuration () in
+  let r = Lwt_main.run (Ketrew_client.as_client ~configuration:c ~f:step) in
+  unwrap r
+
