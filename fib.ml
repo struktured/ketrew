7c941339c74a0ff07a101f9cb2d9e486d29f48d9
Leonid Rozenberg <leonidr@gmail.com>
2015-01-28 12:29:58 -0500

    Fib debuging optional

--- fib.ml
+++ fib.ml
@@ -6,24 +6,32 @@ open Ketrew_pervasives ;;
 
 let path = Printf.sprintf "/tmp/fib%d"
 
-let fibonacci_program = function
-  | 0 -> KEDSL.Program.shf "echo computing %d; echo 0 > %s" (path 0)
-  | 1 -> KEDSL.Program.shf "echo computing %d; echo 1 > %s" (path 1)
-  | n -> KEDSL.Program.shf "echo computing %d; echo $((`cat %s` + `cat %s`)) > %s"
-              (path (n - 2)) (path (n - 1)) (path (n - 1))
- 
+let debug = ref true
+let fibonacci_program ?(debug = false) n =
+  let p =
+    if debug then
+      KEDSL.Program.shf "echo computing %d; %s" n
+    else
+      KEDSL.Program.shf "%s"
+  in
+  match n with
+  | 0 -> p (Printf.sprintf "echo 0 > %s" 0 (path 0))
+  | 1 -> p (Printf.sprintf "echo 1 > %s" 1 (path 1))
+  | n -> p (Printf.sprintf "echo $((`cat %s` + `cat %s`)) > %s"
+                        (path (n - 2)) (path (n - 1)) (path (n - 1)))
+
 module KEDSL = Ketrew.EDSL
 
 let rec compute_fib n =
   let process = KEDSL.direct_execution (fibonacci_program n) in
   let name    = Printf.printf "fibonacci (%d)" n in
-  KEDSL.file_target 
+  KEDSL.file_target
     ~dependencies: [compute_fib (n - 1); compute_fib (n - 2);]
     (* Ketrew doesn't have a way of knowing that there will be multiple
        (n-2) cases (one from n and one from n-1)*)
     ~equivalence:`Same_active_condition
     (* But if we specify that targets have the same active condition. *)
-    ~name 
+    ~name
     ~make:process
     (* Then we can specify the same file target. *)
     (path n)
